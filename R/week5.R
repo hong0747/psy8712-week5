3485efbb007d5a920e1fdb29e1c84bcd6b5b384b
Julia Hong <hong0747@umn.edu>
2026-02-23 12:46:20 -0600

    Created ABclean_tbl

--- R/week5.R
+++ R/week5.R
@@ -15,3 +15,10 @@ Aclean_tbl <- Adata_tbl %>%
   mutate(across(q1:q5, as.integer)) %>%
   left_join(Anotes_tbl, by = "parnum") %>%
   filter(is.na(notes))
+ABclean_tbl <- Bdata_tbl %>%
+  mutate(datadate = as.POSIXct(datadate, format = "%b %d %Y, %H:%M:%S")) %>%
+  mutate(across(q1:q10, as.integer)) %>%
+  left_join(Bnotes_tbl, by = "parnum") %>%
+  filter(is.na(notes)) %>%
+  bind_rows(A = Aclean_tbl, B = ., .id = "lab") %>%
+  select(-notes)
